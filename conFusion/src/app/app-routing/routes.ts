3e7f2a19a546bc3cd8b5da4d42cd50e9df6a5623
Farha Maskuriy <farha.maskuriy@gmail.com>
2019-08-01 15:32:18 +0800

    Single Page Applications Part 2

--- conFusion/src/app/app-routing/routes.ts
+++ conFusion/src/app/app-routing/routes.ts
@@ -21,6 +21,10 @@ export const routes: Routes = [
         component: ContactComponent
     },
     {
+        path: 'dishdetail/:id', 
+        component: DishdetailComponent
+    },
+    {
         path: '', 
         redirectTo: '/home', 
         pathMatch: 'full'
--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+
 
 @Component({
   selector: 'app-dishdetail',
@@ -8,17 +12,17 @@ import { Dish } from '../shared/dish';
 })
 export class DishdetailComponent implements OnInit {
 
-  @Input()
-  dish = Dish;
+  dish: Dish;
 
-  constructor() { }
+  constructor( private dishService: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
-
-    // let thecomment = this.dish['comments'];
-    // for (let i = 0, len = thecomment.length; i < len; i++){
-    //   console.log(thecomment [i]);
-    // }
-  
+    let id = this.route.snapshot.params['id'];
+    this.dish = this.dishService.getDish(id);
+  }
+  goBack(): void {
+    this.location.back();
   }
 }
