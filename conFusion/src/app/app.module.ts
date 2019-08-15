305739cd25331aa852eff4633af1358bf97a0b3b
Farha Maskuriy <farha.maskuriy@gmail.com>
2019-08-15 11:03:40 +0800

    Directive

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { baseURL } from './shared/baseurl';
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
+import { HighlightDirective } from './directives/highlight.directive';
 
 
 @NgModule({
@@ -41,7 +42,8 @@ import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
