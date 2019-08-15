d9a7c0f042571d91cd49e3a9b59ffc166760bc79
Farha Maskuriy <farha.maskuriy@gmail.com>
2019-08-15 12:10:11 +0800

    Animations Part 2

--- conFusion/src/app/about/about.component.ts
+++ conFusion/src/app/about/about.component.ts
@@ -3,11 +3,21 @@ import { Location } from '@angular/common';
 import { LeaderService } from '../services/leader.service';
 import { ActivatedRoute } from '@angular/router';
 import { Leader } from '../shared/leader';
+import { flyInOut, expand } from '../animations/app.animation';
+
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+    },
+    animations: [
+      flyInOut(),
+      expand()
+    ]
 })
 export class AboutComponent implements OnInit {
 
